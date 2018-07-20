031e373b0cf57becf7bfe78895094db2bea52309
Stephen Asbury <sasbury@synadia.com>
2018-07-20 15:13:04 -0700

    added reconnect buffer and wait examples

---
+++ src/main/java/io/nats/examples/ReconnectFiveMB.java
@@ -0,0 +1,26 @@
+package io.nats.examples;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+
+public class ReconnectFiveMB {
+    public static void main(String[] args) {
+
+        try {
+            // [begin reconnect_5mb]
+            Options options = new Options.Builder().
+                                        server("nats://demo.nats.io:4222").
+                                        reconnectBufferSize(5 * 1024 * 1024).
+                                        build();
+            Connection nc = Nats.connect(options);
+
+            // Do something with the connection
+
+            nc.close();
+            // [end reconnect_5mb]
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
