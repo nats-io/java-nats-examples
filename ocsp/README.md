c4e86563b9fc19ca577581a3594dd4d2532b2d1a
scottf <scottfauerbach@gmail.com>
2021-07-02 18:04:17 -0400

    ocsp

---
+++ ocsp/README.md
@@ -0,0 +1,62 @@
+![NATS](../images/large-logo.png)
+
+# OCSP Stapling SSLContext
+
+OCSP Stapling is supported in Java since JDK 8. There is an excellent document from Oracle which describes it in detail.
+
+[Client-Driven OCSP and OCSP Stapling](https://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/ocsp.html)
+
+### Entire VM Approach
+
+It's actually trivial to turn on Client Side OCSP Revocation checking. It's as simple at this code, adding System Properties:
+
+```java
+System.setProperty("jdk.tls.client.enableStatusRequestExtension", "true");
+System.setProperty("com.sun.net.ssl.checkRevocation", "true");
+```
+
+The caveat here is that these properties apply to every single SSLContext running in that VM. 
+These properties are checked at runtime, each time a TLS handshake is made, so it cannot be turned on
+to create an `SSLContext` then turned off. If it is turned off, revocation checking will not happen. 
+
+The example `getVmWideContext()` shows how to make a Vm-Wide SSLContext. See the [OCSP Example Class](main/java/io/nats/ocsp/OcspExample.java)
+
+### Siloed Approach
+
+If it's that easy to turn on OCSP stapling with revocation, why do we need the example?
+
+Consider 3 different types of connections.
+
+1. Standard TLS
+2. OCSP with Revocation checking (OCSP W/REV)
+3. OCSP with no Revocation checking (OCSP NO REV)
+
+It appears that setting the System Properties does not affect STD-TLS certificate handshakes.
+Find your configuration in this table to see if you have to set the System Properties.
+
+| Standard TLS | OCSP W/REV | OCSP NO REV | `enableStatusRequestExtension` | `checkRevocation` |
+| --- | --- | --- | --- | --- |
+| Yes | No | No | false | false |
+| Yes | Yes | No | true | true |
+| Yes | No | Yes | true | false |
+| No | No | No | false | false |
+| No | Yes | No | true | true |
+| No | No | Yes | true | false |
+| Yes | Yes | Yes | true | __siloed__ |
+| No | Yes | Yes | true | __siloed__ |
+
+Please be aware. There are a few examples on the internet that guided development example, in fact it is almost identical to those,
+but none of them set the `enableStatusRequestExtension` flag. As far as we can tell it simply does not work without setting the flag!
+
+The example `getSiloedContext()` shows how to make a siloed SSLContext that does revocation checking. See the [OCSP Example Class](main/java/io/nats/ocsp/OcspExample.java)
+
+## Licenses
+
+This example leverages the [SSLContext Kickstart project](https://github.com/Hakky54/sslcontext-kickstart)
+which operates under the [Apache License 2.0](https://github.com/Hakky54/sslcontext-kickstart/blob/master/LICENSE)
+Their library had some functionality scoped `private` which we needed to use directly so we copied it and made it public.
+This usage is allowed by Apache 2.0 license policy, but we are noting it here for full transparency.
+
+That library and this example both use [Bouncy Castle](https://www.bouncycastle.org/) which has this [Licence](https://www.bouncycastle.org/license.html), an [MIT License](https://opensource.org/licenses/MIT)
+
+Unless otherwise noted, NATS source files are distributed under [Apache Version 2.0](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ ocsp/src/main/java/io/nats/ocsp/OcspExample.java
@@ -0,0 +1,197 @@
+package io.nats.ocsp;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+import nl.altindag.ssl.exception.CertificateParseException;
+import nl.altindag.ssl.exception.GenericIOException;
+import nl.altindag.ssl.util.IOUtils;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.X509TrustedCertificateBlock;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.function.Function;
+
+public class OcspExample
+{
+    private static final String OCSP_CLIENT_CERT = "client-cert.pem";
+    private static final String OCSP_CLIENT_KEY = "client-key.pem";
+    private static final String OCSP_CA_CERT = "ca-cert.pem";
+    private static final String KEY_STORE_PASSWORD = "kspassword";
+    private static final String KEYSTORE_TYPE = "PKCS12";
+    private static final BouncyCastleProvider BOUNCY_CASTLE_PROVIDER = new BouncyCastleProvider();
+    private static final JcaX509CertificateConverter CERTIFICATE_CONVERTER = new JcaX509CertificateConverter().setProvider(BOUNCY_CASTLE_PROVIDER);
+
+    public static void main(String[] args) throws Exception {
+        connect(getSiloedContext());
+        connect(getVmWideContext());
+    }
+
+    public static SSLContext getVmWideContext() {
+        System.setProperty("jdk.tls.client.enableStatusRequestExtension", "true");
+        System.setProperty("com.sun.net.ssl.checkRevocation", "true");
+
+        X509ExtendedKeyManager keyManager =
+                nl.altindag.ssl.util.PemUtils.loadIdentityMaterial(ocspClientCertPath(), ocspClientKeyPath());
+
+        X509ExtendedTrustManager trustManager =
+                nl.altindag.ssl.util.PemUtils.loadTrustMaterial(ocspCaCertPath());
+
+        nl.altindag.ssl.SSLFactory sslFactory = nl.altindag.ssl.SSLFactory.builder()
+                .withIdentityMaterial(keyManager)
+                .withTrustMaterial(trustManager)
+                .build();
+
+        return sslFactory.getSslContext();
+    }
+
+    public static SSLContext getSiloedContext() throws Exception {
+        System.setProperty("jdk.tls.client.enableStatusRequestExtension", "true");
+        System.setProperty("com.sun.net.ssl.checkRevocation", "false");
+
+        X509ExtendedKeyManager keyManager =
+                nl.altindag.ssl.util.PemUtils.loadIdentityMaterial(ocspClientCertPath(), ocspClientKeyPath());
+
+        List<X509Certificate> certificates = loadCertificates(ocspCaCertPath());
+
+        KeyStore trustStore = KeyStore.getInstance(KEYSTORE_TYPE);
+        trustStore.load(null, KEY_STORE_PASSWORD.toCharArray());
+        for (X509Certificate certificate : certificates) {
+            String alias = certificate.getSubjectX500Principal().getName();
+            trustStore.setCertificateEntry(alias, certificate);
+        }
+
+        CertPathBuilder cpb = CertPathBuilder.getInstance("PKIX");
+        PKIXRevocationChecker rc = (PKIXRevocationChecker)cpb.getRevocationChecker();
+        rc.setOptions(EnumSet.of(PKIXRevocationChecker.Option.NO_FALLBACK));
+
+        PKIXBuilderParameters pkixParams = new PKIXBuilderParameters(trustStore, new X509CertSelector());
+        pkixParams.addCertPathChecker(rc);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(new CertPathTrustManagerParameters(pkixParams));
+
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(new KeyManager[]{keyManager}, tmf.getTrustManagers(), new SecureRandom());
+
+        return sslContext;
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    // Try to connect to the server
+    // ----------------------------------------------------------------------------------------------------
+    public static void connect(SSLContext sslContext) {
+        Options options = new Options.Builder()
+                .server("tls://localhost:4222")
+                .maxReconnects(0)
+                .sslContext(sslContext)
+                .build();
+
+        try (Connection nc = Nats.connect(options)) {
+            nc.publish("foo", "bar".getBytes());
+            System.out.println("Connected");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    // Cert Utilities - Leverages
+    // ----------------------------------------------------------------------------------------------------
+    public static Path ocspClientCertPath() {
+        return getOcspCertFilePath(OCSP_CLIENT_CERT);
+    }
+
+    public static Path ocspClientKeyPath() {
+        return getOcspCertFilePath(OCSP_CLIENT_KEY);
+    }
+
+    public static Path ocspCaCertPath() {
+        return getOcspCertFilePath(OCSP_CA_CERT);
+    }
+
+    public static Path getOcspCertFilePath(String fileName) {
+        ClassLoader classLoader = OcspExample.class.getClassLoader();
+        String spec = "nats-server/test/configs/certs/ocsp/" + fileName;
+        //noinspection ConstantConditions
+        return new File(classLoader.getResource(spec).getFile()).toPath();
+    }
+
+    public static List<X509Certificate> loadCertificates(Path... certificatePaths) {
+        List<X509Certificate> certificates = loadCertificate(certificatePath -> {
+            try {
+                return Files.newInputStream(certificatePath, StandardOpenOption.READ);
+            } catch (IOException exception) {
+                throw new GenericIOException(exception);
+            }
+        }, certificatePaths);
+        return certificates;
+    }
+
+    private static <T> List<X509Certificate> loadCertificate(Function<T, InputStream> resourceMapper, T[] resources) {
+        List<X509Certificate> certificates = new ArrayList<>();
+        for (T resource : resources) {
+            try(InputStream certificateStream = resourceMapper.apply(resource)) {
+                certificates.addAll(parseCertificate(certificateStream));
+            } catch (Exception e) {
+                throw new GenericIOException(e);
+            }
+        }
+
+        return Collections.unmodifiableList(certificates);
+    }
+
+    private static List<X509Certificate> parseCertificate(InputStream certificateStream) {
+        String content = IOUtils.getContent(certificateStream);
+        return parseCertificate(content);
+    }
+
+    private static List<X509Certificate> parseCertificate(String certContent) {
+        try {
+            Reader stringReader = new StringReader(certContent);
+            PEMParser pemParser = new PEMParser(stringReader);
+            List<X509Certificate> certificates = new ArrayList<>();
+
+            Object object = pemParser.readObject();
+            while (object != null) {
+                if (object instanceof X509CertificateHolder) {
+                    X509Certificate certificate = CERTIFICATE_CONVERTER.getCertificate((X509CertificateHolder) object);
+                    certificates.add(certificate);
+                } else if (object instanceof X509TrustedCertificateBlock) {
+                    X509CertificateHolder certificateHolder = ((X509TrustedCertificateBlock) object).getCertificateHolder();
+                    X509Certificate certificate = CERTIFICATE_CONVERTER.getCertificate(certificateHolder);
+                    certificates.add(certificate);
+                }
+
+                object = pemParser.readObject();
+            }
+
+            pemParser.close();
+            stringReader.close();
+
+            if (certificates.isEmpty()) {
+                throw new CertificateParseException("Received an unsupported certificate type");
+            }
+
+            return certificates;
+        } catch (IOException | CertificateException e) {
+            throw new CertificateParseException(e);
+        }
+    }
+
+}
