22a3dc276752e2d3e723139875b0e02c4c7c13ce
scottf <scottfauerbach@gmail.com>
2022-03-07 13:03:32 -0500

    JsTimingExample removed and otehr cleanup

--- starter-maven/src/main/java/com/myco/natsapp/App.java
+++ starter-maven/src/main/java/com/myco/natsapp/App.java
@@ -10,6 +10,8 @@ public class App
     public static void main( String[] args )
     {
         try (Connection nc = Nats.connect("nats://localhost:4222")) {
+            System.out.println("Client: " + Nats.CLIENT_VERSION);
+            System.out.println("Server: " + nc.getServerInfo().getVersion());
         }
         catch (InterruptedException e) {
         }
